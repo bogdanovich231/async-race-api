1874b73c2d09cb134de26eaacc16e8309c051554
Tatsiana <kulinkovich56@gmail.com>
2023-08-17 15:30:34 +0200

    fix: index

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
-import Garage from "./components/Garage";
-import Winners from './components/Winners';
-import './global.css'
-
-class App {
-    private garage: Garage;
-
-    private winners: Winners;
-
-    constructor() {
-        this.garage = new Garage();
-        this.winners = new Winners();
-    }
-}
+import './components/Garage';
+import './components/Winners';
+import './global.css';
